90db11a19bcf87f28243e331986c6481b2f3a6a7
Satpreet <golukherasat@gmail.com>
2023-03-06 16:47:08 +0530

    initial

---
+++ src/Data.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+    {
+      id: 1,
+      name: "Sanyam Modi",
+      username: "xyz4",
+    },
+    {
+      id: 2,
+      name: "Riya Gupta",
+      username: "sweety",
+    },
+    {
+      id: 3,
+      name: "Manish Khandelwal",
+      username: "clem",
+    },
+    {
+      id: 4,
+      name: "Satpreet Singh Khera",
+      username: "Asielll",
+    },
+    {
+      id: 5,
+      name: "Mansi",
+      username: "Kamren",
+    },
+    {
+      id: 6,
+      name: "Rahul Roy",
+      username: "xyz2",
+    },
+    {
+      id: 7,
+      name: "Mrs. Gupta",
+      username: "xyz1",
+    },
+    {
+      id: 8,
+      name: "Rohit Mathur",
+      username: "louis",
+    },
+  ];
